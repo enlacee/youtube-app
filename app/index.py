ce3e2eea60c1ce1d3a3a5c3f75e78c9c31fc1e39
Anibal C <detops.tk@gmail.com>
2016-05-02 01:26:15 -0500

    update files

--- app/index.py
+++ app/index.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, session, g, redirect, url_for, abort, \
+from flask import Flask, json, jsonify, request, session, g, redirect, url_for, abort, \
 	render_template, flash
 app = Flask(__name__)
 
@@ -9,6 +9,17 @@ def main():
 	title = "Python request"
 	return render_template('index.html',**locals())
 
+@app.route('/hello')
+def hello():
+	return 'Hello World'
+
+@app.route("/json", methods=['GET'])
+def getdata():
+	return json.dumps({'html':'<span>All fields good !!</span>'})
+
+@app.route('/jsonify')
+def get_current_user():
+    return jsonify(username="pepe rios", email="peperios@gmil.com", id="2343")
 
 if __name__ == "__main__":
 	app.debug = True
