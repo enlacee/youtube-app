8f116448f48d6eb58dd57440061a077546e7882b
Anibal C <detops.tk@gmail.com>
2016-05-20 01:00:18 -0500

    upda

--- app/index.py
+++ app/index.py
@@ -1,5 +1,15 @@
-from flask import Flask, redirect, url_for, session,jsonify, render_template
+from flask import Flask, redirect, url_for, session,jsonify, render_template, request
 from flask_oauth import OAuth
+
+from flask.ext.mysqldb import MySQL
+
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = '123456'
+app.config['MYSQL_DB'] = 'app_youtube'
+app.config['MYSQL_HOST'] = 'localhost'
+mysql.init_app(app)
 
 
 # You must configure these 3 values from Google APIs console
@@ -12,7 +22,7 @@ REDIRECT_URI = '/authorized'  # one of the Redirect URIs from Google APIs consol
 SECRET_KEY = 'development key'
 DEBUG = True
 
-app = Flask(__name__)
+# app = Flask(__name__)
 app.debug = DEBUG
 app.secret_key = SECRET_KEY
 oauth = OAuth()
@@ -32,6 +42,20 @@ google = oauth.remote_app('google',
 @app.route('/')
 def home():
 	return render_template('index.html')
+
+@app.route('/savedata')
+def savedata():
+	idFavorite = request.args.get('idFavorite')
+	cur = mysql.connection.cursor()
+	cur.execute('''SELECT user, host FROM mysql.user''')
+	#
+	query = "INSERT INTO scrap (id_favorite) VALUES ('%s')" % (idFavorite)
+	cur.execute(query)
+	mysql.connection.commit()
+	#
+	rv = cur.fetchall()
+	return str(rv)
+
 
 @app.route('/auth')
 def index():
@@ -63,8 +87,6 @@ def login():
     callback=url_for('authorized', _external=True)
     return google.authorize(callback=callback)
 
-
-
 @app.route(REDIRECT_URI)
 @google.authorized_handler
 def authorized(resp):
@@ -72,14 +94,12 @@ def authorized(resp):
     session['access_token'] = access_token, ''
     return redirect(url_for('index'))
 
-
 @google.tokengetter
 def get_access_token():
     return session.get('access_token')
 
-
 def main():
-    app.run()
+	app.run()
 
 
 if __name__ == '__main__':
--- app/static/js/app.js
+++ app/static/js/app.js
@@ -1,13 +1,56 @@
 
 // Global module
-var myApp = function(){
+function AppScrap(){
+	this.API_KEY = 'AIzaSyAYsa0ljjyuQwSX1LQDwQ1WRlXiBVCwOKI';
+	this.model = new AjaxModel();
 
-	var local = function(){
-		return 1+1;
+	this.getApiKey = function() {
+		return this.API_KEY;
 	};
 
-	return {
-		sumar : local,
-	};
+}
+var AS = AppScrap.prototype;
 
-}();
+/*
+* get api youtube Datos basicos
+*/
+AS.getDatosBasicosYoutube = function(access_token){
+	var me = this;
+	var url = "https://www.googleapis.com/youtube/v3/channels?part=snippet,contentDetails,brandingSettings,invideoPromotion&mine=true&key=" + me.getApiKey();
+	url += '&access_token=' + access_token;
+	me.model.load(url)
+		.then(function() {
+			console.log('me', me);
+			console.log(me.model);
+			console.log("id favorito");
+			console.log("Name", me.model.items[0].snippet.title);
+			console.log("URL avatar", me.model.items[0].snippet.thumbnails.high.url);
+			console.log("ID favoritas", me.model.items[0].contentDetails.relatedPlaylists.favorites);
+			var idFavorite = me.model.items[0].contentDetails.relatedPlaylists.favorites;
+
+
+			$.ajax({
+			  method: "GET",
+			  url: "/savedata",
+			  data: { idFavorite: idFavorite }
+			})
+			  .done(function( msg ) {
+			    alert( "Data Saved: " + msg );
+			  });
+
+		});
+};
+
+
+
+
+
+var app = new AppScrap();
+//app.testOne();
+
+if (Util.getParameterByName('access_token')) {
+	console.log("API youtube");
+	var access_token = Util.getParameterByName('access_token');
+	console.log('access_token', access_token);
+	app.getDatosBasicosYoutube(access_token);
+}
